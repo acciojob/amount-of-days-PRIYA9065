494ec29df6b4f9e40c8a994d40c4e46d8d98707a
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-08-30 00:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const year = prompt("Enter the year");
+function daysOfAYear(year){
+	if(year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)){
+		alert("366");
+	}
+
+	else{
+		alert("365");
+	}
+}
+daysOfAYear(year)
